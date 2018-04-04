3b5e3d7e8b33342a03aecc2b1169758486410034
Matt Styles <matt@personalurban.com>
2018-04-04 21:31:07 +0100

    add pixel image component

--- packages/app/src/views/app.jsx
+++ packages/app/src/views/app.jsx
@@ -1,9 +1,13 @@
 
+import React, {Fragment} from 'react'
 import Title from 'components/title'
 import styled from 'styled-components'
 import oc from 'open-color'
 
 import {signal} from 'signals'
+
+import {Image} from '../components/image'
+import imagePath from '../assets/charb.png'
 
 const Button = styled.button`
   font-family: 'source code pro';
@@ -23,12 +27,30 @@ const update = event => {
 }
 
 const App = ({state}) => (
-  <div>
+  <Fragment>
     <Title>
       {`chaos-remake: ${state.message}`}
     </Title>
     <Button onClick={update}>Click me</Button>
-  </div>
+    <Image
+      url={imagePath}
+      scale={16}
+      size={8}
+      u={0}
+      v={0}
+    />
+    <div style={{position: 'relative'}}>
+      <Image
+        url={imagePath}
+        scale={16}
+        size={8}
+        u={1}
+        v={1}
+        x={200}
+        y={200}
+      />
+    </div>
+  </Fragment>
 )
 
 export default App
